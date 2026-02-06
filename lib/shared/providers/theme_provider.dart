7fd64d7c2a4b7d1d302f269dd01b7e8a29ebc546
Hugo <khachong2102005@gmail.com>
2026-02-07 05:59:58 +0700

    add shared providers

---
+++ lib/shared/providers/theme_provider.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import '../services/storage_service.dart';
+import '../../injection_container.dart' as di;
+
+class ThemeProvider extends ChangeNotifier {
+  final StorageService _storageService = di.sl<StorageService>();
+  
+  ThemeMode _themeMode = ThemeMode.system;
+  
+  ThemeMode get themeMode => _themeMode;
+  
+  bool get isDarkMode {
+    if (_themeMode == ThemeMode.system) {
+      return WidgetsBinding.instance.platformDispatcher.platformBrightness == Brightness.dark;
+    }
+    return _themeMode == ThemeMode.dark;
+  }
+  
+  ThemeProvider() {
+    _loadThemeMode();
+  }
+  
+  Future<void> _loadThemeMode() async {
+    final savedTheme = await _storageService.getThemeMode();
+    if (savedTheme != null) {
+      switch (savedTheme) {
+        case 'light':
+          _themeMode = ThemeMode.light;
+          break;
+        case 'dark':
+          _themeMode = ThemeMode.dark;
+          break;
+        case 'system':
+        default:
+          _themeMode = ThemeMode.system;
+          break;
+      }
+      notifyListeners();
+    }
+  }
+  
+  Future<void> setThemeMode(ThemeMode themeMode) async {
+    _themeMode = themeMode;
+    
+    String themeModeString;
+    switch (themeMode) {
+      case ThemeMode.light:
+        themeModeString = 'light';
+        break;
+      case ThemeMode.dark:
+        themeModeString = 'dark';
+        break;
+      case ThemeMode.system:
+        themeModeString = 'system';
+        break;
+    }
+    
+    await _storageService.saveThemeMode(themeModeString);
+    notifyListeners();
+  }
+  
+  Future<void> toggleTheme() async {
+    if (_themeMode == ThemeMode.light) {
+      await setThemeMode(ThemeMode.dark);
+    } else {
+      await setThemeMode(ThemeMode.light);
+    }
+  }
+}
